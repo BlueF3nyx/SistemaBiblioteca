78a3162ab0fd4fa641167e2bf495edaa55e120ac
Pedro Henrique <BlueF3ny>
2025-05-23 11:19:25 -0300

    criação do modelo MVVM e criação da verificação de Livros

---
+++ Models/Emprestimo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBiblioteca.Models
+{
+    public class Emprestimo
+    {
+        public int ID { get; set; }
+        public int ID_Livro { get; set; }
+        public int ID_Membro { get; set; }
+        public DateTime Data_Emprestimo { get; set; }
+        public DateTime? Data_Devolucao { get; set; }
+        public string Status { get; set; } // Visualizar os Empréstimos: "Emprestado", "Devolvido", "Atrasado"
+    }
+}
---
+++ Models/Membro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBiblioteca.Models
+{
+    public class Membro
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
