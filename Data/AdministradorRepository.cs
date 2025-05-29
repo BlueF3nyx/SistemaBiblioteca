d69bfceb0edce6e44bde433b7c397971cb36f738
Pedro Henrique <BlueF3ny>
2025-05-29 20:28:58 -0300

    criação de CRUDS

---
+++ Data/AdministradorRepository.cs
@@ -0,0 +1,101 @@
+﻿using SistemaBiblioteca.Models;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace SistemaBiblioteca.Data
+{
+    public class AdministradorRepository
+    {
+        public List<Administrador> ObterTodos()
+        {
+            List<Administrador> lista = new List<Administrador>();
+            using MySqlConnection conn = new MySqlConnection(DatabaseConfig.ConnectionString);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM administrador", conn);
+            using MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                lista.Add(new Administrador
+                {
+                    Id = reader.GetInt32("id"),
+                    Nome = reader.GetString("nome"),
+                    Email = reader.GetString("email"),
+                    Senha = reader.GetString("senha")
+                });
+            }
+
+            return lista;
+        }
+
+        public Administrador ObterPorId(int id)
+        {
+            Administrador admin = null;
+            using MySqlConnection conn = new MySqlConnection(DatabaseConfig.ConnectionString);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM administrador WHERE id_administrador = @id", conn);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            using MySqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                admin = new Administrador
+                {
+                    Id = reader.GetInt32("id_administrador"),
+                    Nome = reader.GetString("nome"),
+                    Email = reader.GetString("email"),
+                    Senha = reader.GetString("senha")
+                };
+            }
+
+            return admin;
+        }
+
+        public void Adicionar(Administrador administrador)
+        {
+            using MySqlConnection conn = new MySqlConnection(DatabaseConfig.ConnectionString);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand(@"
+                INSERT INTO administrador (nome, email, senha)
+                VALUES (@nome, @email, @senha)", conn);
+
+            cmd.Parameters.AddWithValue("@nome", administrador.Nome);
+            cmd.Parameters.AddWithValue("@email", administrador.Email);
+            cmd.Parameters.AddWithValue("@senha", administrador.Senha);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public void Atualizar(Administrador administrador)
+        {
+            using MySqlConnection conn = new MySqlConnection(DatabaseConfig.ConnectionString);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand(@"
+                UPDATE administrador
+                SET nome = @nome,
+                    email = @email,
+                    senha = @senha
+                WHERE id_administrador = @id", conn);
+
+            cmd.Parameters.AddWithValue("@nome", administrador.Nome);
+            cmd.Parameters.AddWithValue("@email", administrador.Email);
+            cmd.Parameters.AddWithValue("@senha", administrador.Senha);
+            cmd.Parameters.AddWithValue("@id", administrador.Id);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public void Remover(int id)
+        {
+            using MySqlConnection conn = new MySqlConnection(DatabaseConfig.ConnectionString);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand("DELETE FROM administrador WHERE id_administrador = @id", conn);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
--- Models/Administrador.cs
+++ Models/Administrador.cs
@@ -8,8 +8,8 @@ namespace SistemaBiblioteca.Models
 {
     public class Administrador
     {
-        public int ID {  get; set; }
-        public required string Name { get; set; }
+        public int Id {  get; set; }
+        public required string Nome { get; set; }
         public required string Email { get; set; }
 
         public required string Senha { get; set; }
